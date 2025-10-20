d5fa8c24985678f705d3bfaeee2ca0984cb9ed04
utmmyppol-glitch <utmmyppol@gmail.com>
2025-10-20 20:06:30 +0900

    상품 관리 완성했습니다

---
+++ src/main/java/com/project/gugumarket/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.project.gugumarket.repository;
+
+import com.project.gugumarket.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    // src/main/java/com/gugu/market/domain/category/CategoryRepository.java
+
+
+
+
+}
